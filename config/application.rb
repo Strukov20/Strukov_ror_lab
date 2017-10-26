95dcd02a9f0720e77e6b6bbd15cf96a79db02862
Unknown <bodya19981997@gmail.com>
2017-10-26 21:17:07 +0300

    back

--- config/application.rb
+++ config/application.rb
@@ -5,15 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-# Bower asset paths
-root.join('vendor', 'assets', 'bower_components').to_s.tap do |bower_path|
-  config.sass.load_paths << bower_path
-  config.assets.paths << bower_path
-end
-# Precompile Bootstrap fonts
-config.assets.precompile << %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
-# Minimum Sass number precision required by bootstrap-sass
-::Sass::Script::Value::Number.precision = [8, ::Sass::Script::Value::Number.precision].max
+
 module StrukovRor
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
